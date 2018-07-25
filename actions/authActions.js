387e792cbaa7a58f640057bc8f3783878bceeef7
Hung.Nguyen <ntmhung94@gmail.com>
2018-07-25 23:38:31 +0700

    update facebook login function

--- actions/authActions.js
+++ actions/authActions.js
@@ -1,20 +1,39 @@
 /**
  * Created by minhhung on 7/23/18.
  */
-import {AsyncStorage} from 'react-native';
-import {
-    FACEBOOK_LOGIN_SUCCESS
-} from './type';
+import {AsyncStorage} from "react-native";
+import {Facebook} from "expo";
+import {FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_FAIL} from "./type";
 
 /**
  * AsyncStorage is used to access mobile storage
  * It is similar to localStorage on browser
+ *
+ * This facebookLogin function return a function, therefore we in the arrow function, we immediately return aync
+ * function with "dispatch" parameter
  */
 export const facebookLogin = () => async dispatch => {
-        let fb_token = await AsyncStorage.getItem('fb_token');
-        if(fb_token){
+    let fb_token = await AsyncStorage.getItem('fb_token');
 
-        } else {
+    if (fb_token) {
+        dispatch({type: FACEBOOK_LOGIN_SUCCESS, payload: fb_token})
+    } else {
+        doFacebookLogin(dispatch);
+    }
+};
 
-        }
-    };
+const doFacebookLogin = async dispatch => {
+    let {type, token} = await Facebook.logInWithReadPermissionsAsync('273826823374762', {
+        permissions: ['public_profile']
+    });
+
+    if (type === 'cancel') {
+        return dispatch({
+            type: FACEBOOK_LOGIN_FAIL
+        })
+    }
+
+    await AsyncStorage.setItem('fb_token', token);
+
+    dispatch({type: FACEBOOK_LOGIN_SUCCESS, payload: token})
+};
--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -2,9 +2,18 @@
  * Created by minhhung on 7/15/18.
  */
 import React, {Component} from "react";
-import {StyleSheet, View, Text} from "react-native";
+import {AsyncStorage, View, Text} from "react-native";
+import {connect} from 'react-redux';
+import {facebookLogin} from '../actions';
+
 
 class AuthScreen extends Component {
+
+    componentDidMount(){
+        this.props.facebookLogin();
+        AsyncStorage.removeItem('fb_token');
+    }
+
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -20,4 +29,6 @@ class AuthScreen extends Component {
     }
 }
 
-export default AuthScreen;
+
+
+export default connect(null, facebookLogin)(AuthScreen);
