f90fb1cf31992908a98c24f517d4fcdd778c9a61
Hung.Nguyen <ntmhung94@gmail.com>
2018-07-28 21:18:07 +0700

    - update git ignore - init map screen

---
+++ actions/jobActions.js
@@ -0,0 +1,9 @@
+/**
+ * Created by minhhung on 7/28/18.
+ */
+import axios from "axios";
+import {FETCH_JOBS} from "./type";
+
+export const fetchJobs = region => async dispatch => {
+
+};
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,13 +2,43 @@
  * Created by minhhung on 7/15/18.
  */
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ActivityIndicator} from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+    state = {
+        mapLoaded: false,
+        region: {
+            latitude: 37,
+            longitude: -122,
+            latitudeDelta: 0.09,
+            longitudeDelta: 0.04
+        }
+    };
+
+    componentDidMount(){
+        this.setState({mapLoaded: true});
+    }
+
+    onRegionChangeComplete = (region) => {
+        this.setState({region})
+    };
+
     render(){
+        if(!this.state.mapLoaded){
+            return (
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                    <ActivityIndicator size="large"/>
+                </View>
+            );
+        }
         return (
-            <View>
-                <Text>MapScreen</Text>
+            <View style={{flex: 1}}>
+                <MapView
+                    style={{flex: 1}}
+                    region={this.state.region}
+                    onRegionChangeComplete={this.onRegionChangeComplete}
+                />
             </View>
         )
     }
